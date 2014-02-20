4d78fcbf7868877b03ac0420d5b503f471fb30c2
Ulhas Mandrawadkar <ulhas@justchalo.com>
2014-02-20 16:18:29 +0530

    Initial Commit

---
+++ UICustomNavigationController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UICustomNavigationController
+//
+//  Created by Ulhas Mandrawadkar on 2/20/14.
+//  Copyright (c) 2014 Ulhas Mandrawadkar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
